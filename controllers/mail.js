83ed98bd67a4f250e9ce596491e3be0b711487c7
Mohdyusufsaifi <95411943+Mohdyusufsaifi@users.noreply.github.com>
2023-05-15 17:59:59 +0000

    Change it now Gmail Authentication

--- controllers/mail.js
+++ controllers/mail.js
@@ -1,15 +1,37 @@
 const express= require('express');
 const app=express();
 const nodemailer = require('nodemailer');
+const { google } = require('googleapis');
 
-exports.sendEmail=(req, res) => {
+// exports.sendEmail=(req, res) => {
+//     const transporter = nodemailer.createTransport({
+//         host: 'smtp.ethereal.email',
+//         port: 587,
+//         auth: {
+//             user: 'frederik.kovacek75@ethereal.email',
+//             pass: 'b1ZVaXG2t6Czt5dyuu'
+//         }
+//     });
+exports.sendEmail=async(req,res)=> {
+    const clientId = '402509665383-akkt7dce3buasdbr2mlho53u9jsvqtvg.apps.googleusercontent.com';
+    const clientSecret = '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';
+    const refreshToken = 'https://oauth2.googleapis.com/token';
+  
+    const oAuth2Client = new google.auth.OAuth2(clientId, clientSecret);
+    oAuth2Client.setCredentials({ refresh_token: refreshToken });
+  
+    const accessToken = await oAuth2Client.getAccessToken();
+  
     const transporter = nodemailer.createTransport({
-        host: 'smtp.ethereal.email',
-        port: 587,
-        auth: {
-            user: 'frederik.kovacek75@ethereal.email',
-            pass: 'b1ZVaXG2t6Czt5dyuu'
-        }
+      service: 'gmail',
+      auth: {
+        type: 'OAuth2',
+        user: 'azeemsaifi38180@gmail.com',
+        clientId: clientId,
+        clientSecret: clientSecret,
+        refreshToken: refreshToken,
+        accessToken: accessToken
+      }
     });
 
     const mailOptions = {

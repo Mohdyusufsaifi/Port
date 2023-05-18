b0ddd835d8588c15d82c772cd9076dd6c128b820
Mohdyusufsaifi <95411943+Mohdyusufsaifi@users.noreply.github.com>
2023-05-18 23:41:02 +0530

    Update mail.js

--- controllers/mail.js
+++ controllers/mail.js
@@ -18,7 +18,7 @@ exports.sendEmail=(req, res) => {
             user: 'azeemsaifi38180@gmail.com',
             clientId: '402509665383-akkt7dce3buasdbr2mlho53u9jsvqtvg.apps.googleusercontent.com',
             clientSecret: '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',
-            accessToken:'ya29.a0AWY7Ckk1PBwODZUz6U_TBL69zdrcZvWESbrn1ZtM0ibqU8BZb0UoMD6Brm_q3LQUe4UWtuoTZ_0fC99QWS__MH5fJ54AMEhiwPh-fjbjYaU5v_4AjikWvNoqIIG0ZN-uduNWa5d36n4tYXCjigVTQmUEnwgsaCgYKAScSARESFQG1tDrp7VqNJP1rY_OMxavTIxqRtQ0163',
+            accessToken:'ya29.a0AWY7CklIUdtandkwA0f7D2rxb-Tx2XLmJiQhzU09UoYzbgpju_BsoHqmftrJr4SzMa1orylEfrUhQgztdihN_2XRKkt_m6w2SFnBWoVhWkmt4zat3hH-nG4BtuRjkacCz5Oea-e1ZHPNeCzka0wGS-_TpU9CaCgYKAe0SARESFQG1tDrpcEZ2SdYpYa6eF2uJqNSGHQ0163',
         }
     });
 

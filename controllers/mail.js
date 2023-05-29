70efe5bfaccaa0c95910fe51da6163cf9d3fa772
Mohdyusufsaifi <95411943+Mohdyusufsaifi@users.noreply.github.com>
2023-05-30 00:33:08 +0530

    Update mail.js
    Total Work in Local Machine

--- controllers/mail.js
+++ controllers/mail.js
@@ -1,28 +1,29 @@
 const nodemailer = require('nodemailer');
 const { google } = require('googleapis');
+const OAuth2 = google.auth.OAuth2;
 
-// exports.sendEmail=(req, res) => {
-//     const transporter = nodemailer.createTransport({
-//         host: 'smtp.ethereal.email',
-//         port: 587,
-//         auth: {
-//             user: 'frederik.kovacek75@ethereal.email',
-//             pass: 'b1ZVaXG2t6Czt5dyuu'
-//         }
-// //     });
-exports.sendEmail=(req, res) => {
-    const transporter =nodemailer.createTransport({
+exports.sendEmail = async (req, res) => {
+    const oauth2Client = new OAuth2(
+        "402509665383-akkt7dce3buasdbr2mlho53u9jsvqtvg.apps.googleusercontent.com",
+        "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",
+        "https://developers.google.com/oauthplayground"
+   );
+    oauth2Client.setCredentials(
+        {
+            refresh_token:"1//04E9kR6QXWw6bCgYIARAAGAQSNwF-L9Ir6cpQEVtAzbnrAhr3vN3RAOuuLGCMItq1AYZlD2j7YEP3D4j0-RIUImk0ml2D4lGTqVc"
+        });
+    const accessToken = oauth2Client.getAccessToken()
+    const transporter = nodemailer.createTransport({
         service: 'gmail',
         auth: {
             type: 'OAuth2',
             user: 'azeemsaifi38180@gmail.com',
-            clientId: '402509665383-akkt7dce3buasdbr2mlho53u9jsvqtvg.apps.googleusercontent.com',
-            clientSecret: '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',
-            accessToken:'ya29.a0AWY7Ckl4U0V7UXgEaymzzEcpUkSD2X1IJmJCFyXlUpkAFSQyng6mhAob7Vue84gpBI9BRiljHKalNP7Q2Gm_99wcWEpNnzcQJs3xZaGZJBGA42ZHklzSvAh-xurwHDlk89K8QRZvCEiwaCHZIuC0sCpxWM6YaCgYKAS0SARESFQG1tDrpAc9JwOsYMS4KubIdm8bQCg0163',
-            refreshToken:'1//04vaGrRHb1kbmCgYIARAAGAQSNwF-L9Ir6B4JdpD2TSxyv7pY2NWIjFrZJ5R3TS7_7Oz3hANp9DozNzcxvKHs69m91QdpO5WqWgI',
+            clientId:"402509665383-akkt7dce3buasdbr2mlho53u9jsvqtvg.apps.googleusercontent.com",
+            clientSecret:"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",
+            refreshToken:"1//04E9kR6QXWw6bCgYIARAAGAQSNwF-L9Ir6cpQEVtAzbnrAhr3vN3RAOuuLGCMItq1AYZlD2j7YEP3D4j0-RIUImk0ml2D4lGTqVc",
+            accessToken: accessToken
         }
     });
-
     const mailOptions = {
         from: "frederik.kovacek75@ethereal.email",
         to: "azeemsaifi38180@gmail.com",

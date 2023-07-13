a736b3804fb3f63981e0c7fa2b524bfbb6f2fefe
Mohdyusufsaifi <95411943+Mohdyusufsaifi@users.noreply.github.com>
2023-07-13 22:15:22 +0530

    Update mail.js

--- controllers/mail.js
+++ controllers/mail.js
@@ -3,30 +3,13 @@ const { google } = require('googleapis');
 const OAuth2 = google.auth.OAuth2;
 
 exports.sendEmail = async (req, res) => {
-    const oauth2Client = new OAuth2(
-        "402509665383-akkt7dce3buasdbr2mlho53u9jsvqtvg.apps.googleusercontent.com",
-        "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",
-        "https://developers.google.com/oauthplayground"
-   );
-    oauth2Client.setCredentials(
-        {
-            refresh_token:"1//04crQNYGvTA2QCgYIARAAGAQSNwF-L9IrJH3QHoL1DAnkpgTPHDpd_NnA_CaqqfvYqabPKcGx7c54s78cFrcCZ6EydRWxJD4V_NE"
-        });
-    const accessToken = oauth2Client.getAccessToken()
-    const transporter = nodemailer.createTransport({
-        service: 'gmail',
+    host: "live.smtp.mailtrap.io",
+        port: 587,
         auth: {
-            type: 'OAuth2',
-            user: 'azeemsaifi38180@gmail.com',
-            clientId:"402509665383-akkt7dce3buasdbr2mlho53u9jsvqtvg.apps.googleusercontent.com",
-            clientSecret:"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",
-            refreshToken:"1//04crQNYGvTA2QCgYIARAAGAQSNwF-L9IrJH3QHoL1DAnkpgTPHDpd_NnA_CaqqfvYqabPKcGx7c54s78cFrcCZ6EydRWxJD4V_NE",
-            accessToken: accessToken
-        },
-        tls: {
-            rejectUnauthorized: false
-        }      
-    });
+        user: "Azeem Saifi",
+        pass: "c00d97fbb11d7d02e27210eaa842f3b5"
+    }
+});
     const mailOptions = {
         from: "frederik.kovacek75@ethereal.email",
         to: "azeemsaifi38180@gmail.com",

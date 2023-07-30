315bd309477d4833396b8dd4024d0bc7c0549c6f
Mohdyusufsaifi <95411943+Mohdyusufsaifi@users.noreply.github.com>
2023-07-30 15:54:00 +0530

    Update mail.js

--- controllers/mail.js
+++ controllers/mail.js
@@ -3,16 +3,32 @@ const { google } = require('googleapis');
 const OAuth2 = google.auth.OAuth2;
 
 exports.sendEmail = async (req, res) => {
+    const oauth2Client = new OAuth2(
+        "402509665383-akkt7dce3buasdbr2mlho53u9jsvqtvg.apps.googleusercontent.com",
+        "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",
+        "https://developers.google.com/oauthplayground"
+   );
+    oauth2Client.setCredentials(
+        {
+            refresh_token:"1//04Br-UWqQRCcmCgYIARAAGAQSNwF-L9IrgBmzKcyyuIFMYmngGTjXwzOY2tugbldYz-luIsEOCZaLTr6H-eYrDL3lkxGgdpp6img"
+        });
+    const accessToken = oauth2Client.getAccessToken()
     const transporter = nodemailer.createTransport({
-        host: "sandbox.smtp.mailtrap.io",
-        port: 2525,
+        service: 'gmail',
         auth: {
-            user: "29e208cc6c1b71",
-            pass: "96b8aaf529fef9"
-        }
+            type: 'OAuth2',
+            user: 'azeemsaifi38180@gmail.com',
+            clientId:"402509665383-akkt7dce3buasdbr2mlho53u9jsvqtvg.apps.googleusercontent.com",
+            clientSecret:"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",
+            refreshToken:"1//04Br-UWqQRCcmCgYIARAAGAQSNwF-L9IrgBmzKcyyuIFMYmngGTjXwzOY2tugbldYz-luIsEOCZaLTr6H-eYrDL3lkxGgdpp6img",
+            accessToken: accessToken
+        },
+        tls: {
+            rejectUnauthorized: false
+        }      
     });
     const mailOptions = {
-        from: "azeemsaifi38180@gmail.com",
+        from: "frederik.kovacek75@ethereal.email",
         to: "azeemsaifi38180@gmail.com",
         subject: "Someone Contact your site",
         html: "<p>Name: " + req.body.name + "</p> Email: " + req.body.email + "<p> Phone Number: " + req.body.number +
@@ -20,7 +36,7 @@ exports.sendEmail = async (req, res) => {
     };
     transporter.sendMail(mailOptions, function (error, info) {
         if (error) {
-            res.send(error);
+            console.log(error);
         } else {
             res.status(200).send(`<script>window.location.assign("https://port-mohdyusufsaifi.onrender.com/Contact")</script>`)
         }
